69ace999a43226576710ea7237d420969aeaf996
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-11-28 00:01:00 +0530

    271125

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://default-get-tasks-url',
-    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://default-delete-task-url',
-    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://default-create-task-url',
+    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://gettask.devopsinsight.biz',
+    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://deletetask.devopsinsight.biz',
+    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://addtask.devopsinsight.biz',
   };
   
   export default config;
