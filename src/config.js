e2d296365d6d705f45d9df2b6058ad068b0a0ade
RakeshKSingh29 <singhrakesh2986@gmail.com>
2025-12-02 03:34:40 +0530

    cnfig changed

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://gettask.devopsinsight.biz',
-    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://deletetask.devopsinsight.biz',
-    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://addtask.devopsinsight.biz',
+    GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://gettask.devopsinsight.biz/api',
+    DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://deletetask.devopsinsight.biz/api',
+    CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://addtask.devopsinsight.biz/api',
   };
   
   export default config;
